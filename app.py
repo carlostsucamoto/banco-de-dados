4fc8c54e3d9d41e436cbaa92e377ea9c40489ac3
carlostsucamoto <caduchiarelli@hotmail.com>
2026-04-25 23:27:14 -0300

    feat: mock database connection and implement capture cooldown logic

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
+
 import oracledb
 from datetime import datetime, timedelta
+
+
+ultima_captura = datetime.now() - timedelta(hours=3) 
+
+try:
+    conexao = oracledb.connect(
+        user="admin", 
+        password="123", 
+        dsn="mock_dsn"
+    )
+    print("Conexão estabelecida com sucesso.")
+    
+except oracledb.Error as e:
+    
+    print(f"Erro técnico detectado: {e}")
+
 horario_liberacao = ultima_captura + timedelta(hours=2)
 agora = datetime.now()
-if horario_liberacao >= agora:
-    print("Habilidade em recarga")
-else:
+
+if agora >= horario_liberacao:
     print("Captura Liberada")
+else:
+    print("Habilidade em recarga")
