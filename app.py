cf165cb3dfb77c54b9ebc96d2637eb4fc38839e4
carlostsucamoto <caduchiarelli@hotmail.com>
2026-04-27 23:58:00 -0300

    feat: Implements cooldown business rule and OracleDB connection.

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import oracledb
 import os 
+
+
 load_dotenv()
-from datetime import datetime, timedelta
-
-
-ultima_captura = datetime.now() - timedelta(hours=3) 
-
-
 def obter_conexao():
     conexao = None
     try:
@@ -26,11 +23,26 @@ try:
         meu_cursor = conexao_ativa.cursor()
         meu_cursor.execute("SELECT MAX(HORA_CAPTURA) FROM ALMASPASSAGEIRO WHERE ID_PASSAGEIRO = 1")
         resultado = meu_cursor.fetchone()
-        print(resultado)
-        meu_cursor.close()
+
+
+        if resultado[0] is not None:
+            data_ultima_captura = resultado[0]
+            tempo_passado = datetime.now() - data_ultima_captura
+            tempo_faltando = timedelta(hours=3) - tempo_passado
+
+            if tempo_passado < timedelta(hours=3):
+                print(f"Acesso Negado: Alma ainda não está aqui para ser capturada espere mais {tempo_faltando} horas.")
+            else:
+                print("Alma pronta para ser capturada.")
+
+        else:
+            print ("Alma pronta para ser capturada.")
         
+        meu_cursor.close()       
     else:
         print("Sistema operando em modo degradado. Banco offline.")
+
+    
 
 finally:
     if conexao_ativa is not None:
